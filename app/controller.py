79a9d2dfceeb8981670a8638d4354c5bec1c5ee0
Your Name <mmuzasantuna@gmail.com>
2020-09-25 18:10:23 +0100

    start_game function works, player_2 wins. Function placement is not correct tho

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import render_template
+from app.models.game import player_1, player_2
+from app.models.player import *
 
 @app.route('/rock/scissors')
 def index():
-    return "Players 1 wins by playing rock"
+    return render_template("index.html", players=players)
+
+@app.route('/playing_game')
+def start_game():
+    if player_1.choice == player_2.choice:
+        return None
+    elif player_1.choice == "scissors" and player_2.choice == "rock":
+        return f"{player_2.name} wins"
--- app/models/game.py
+++ app/models/game.py
@@ -1,4 +1,7 @@
 from app.models.player import *
 
-player_1 = Player("Pepe", "rock")
-player_2 = Player("Manoli", "scissors")
+player_1 = Player("Pepe", "scissors")
+player_2 = Player("Manoli", "rock")
+
+players = [player_1, player_2]
+
