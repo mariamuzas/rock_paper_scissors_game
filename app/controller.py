1930819ae926b05413db687e76776a365aba80e9
Your Name <mmuzasantuna@gmail.com>
2020-09-27 10:10:28 +0100

    Rest of the option for plying_game fuction added, base.html and how_to_play.html added

--- app/controller.py
+++ app/controller.py
@@ -5,20 +5,25 @@ from app.models.player import *
 
 @app.route('/list_players')
 def index():
+    player_1 = Player("Pepe", "rock")
+    player_2 = Player("Manoli", "scissors")
     return render_template("index.html", players= [player_1,player_2])
 
 @app.route('/game_1')
 def start_game():
-    player_1 = Player("pepe", "rock")
-    player_2 = Player("manoli", "scissors")
-
+    player_1 = Player("Pepe", "rock")
+    player_2 = Player("Manoli", "scissors")
     result = playing_game(player_1, player_2)
     return render_template("index.html", game_result=result)
 
 @app.route('/<p1_choice>/<p2_choice>')
 def second_game(p1_choice, p2_choice):
-    player_1 = Player("pepe", p1_choice)
-    player_2 = Player("manoli", p2_choice)
+    player_1 = Player("Pepe", p1_choice)
+    player_2 = Player("Manoli", p2_choice)
 
     result = playing_game(player_1, player_2)
-    return render_template("index.html", game_result=result)
+    return render_template("index.html", second_game_result=result)
+
+@app.route('/how_to_play/')
+def intruccions():
+    return render_template("how_play.html")
--- app/models/game.py
+++ app/models/game.py
@@ -1,7 +1,18 @@
 from app.models.player import *
 
 def playing_game(player_1, player_2):
+    while player_1.choice == player_2.choice:
+        return "Nobody wins"
+
     if player_1.choice == "rock" and player_2.choice == "scissors":
+        return f"{player_1.name} wins"
+    elif player_1.choice == "rock" and player_2.choice == "paper":
         return f"{player_2.name} wins"
-    elif player_1.choice == player_2.choice:
-        return "draw"
+    elif player_1.choice == "paper" and player_2.choice == "scissors":
+        return f"{player_2.name} wins"
+    elif player_1.choice == "paper" and player_2.choice == "rock":
+        return f"{player_1.name} wins"
+    elif player_1.choice == "scissors" and player_2.choice == "paper":
+        return f"{player_1.name} wins"
+    elif player_1.choice == "scissors" and player_2.choice == "rock":
+        return f"{player_2.name} wins"
